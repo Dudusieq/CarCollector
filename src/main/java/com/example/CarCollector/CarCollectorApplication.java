b2c4592f7e8ad104be37dd119b8fc15cc6adb6a8
Dudusieq <dudusiek32@gmail.com>
2025-04-29 11:27:09 +0200

    1

---
+++ src/main/java/com/example/CarCollector/CarCollectorApplication.java
@@ -0,0 +1,18 @@
+package com.example.CarCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCollectorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CarCollectorApplication.class, args);
+
+
+
+
+	}
+
+}
