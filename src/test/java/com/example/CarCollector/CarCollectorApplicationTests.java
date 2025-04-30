b74138074bc10b573ec30e62fcc8eabeb398f8ee
Dudusieq <dudusiek32@gmail.com>
2025-04-30 09:24:57 +0200

    5

---
+++ src/test/java/com/example/CarCollector/CarCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarCollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
